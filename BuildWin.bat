7a2c6b4b98c9b8ddd3595df5d4d9a9c839792a26
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2026-05-18 12:04:34 +0200

    Act 1

---
+++ BuildWin.bat
@@ -0,0 +1,4 @@
+Clear.bat
+python -m PyInstaller --onefile --windowed --noconsole --icon=gamble-with-your-friends.ico NoCompatibleToInstall.py
+python -m PyInstaller --onefile --windowed --noconsole --icon=gamble-with-your-friends.ico PlayGame.py
+echo v1.0.4 > GameVersion.txt

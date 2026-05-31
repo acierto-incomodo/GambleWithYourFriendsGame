873ad093136c781974e41901eb5692d4bdde4e3c
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2026-05-31 23:44:46 +0200

    Act 3

--- BuildWin.ps1
+++ BuildWin.ps1
@@ -1,3 +1,3 @@
 ./Clear.ps1
 python -m PyInstaller --onefile --windowed --noconsole --icon=gamble-with-your-friends.ico NoCompatibleToInstall.py
-echo "v1.0.4 ModsBuild v1" > GameVersion.txt
+echo "v1.0.4 ModsBuild v2" > GameVersion.txt

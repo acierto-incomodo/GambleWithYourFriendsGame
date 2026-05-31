71a0d4bbc420e2b4119182da001dd8518a9eedb4
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2026-05-31 22:37:43 +0200

    Act 2

--- BuildWin.bat
+++ BuildWin.bat
@@ -1,4 +1,3 @@
 Clear.bat
 python -m PyInstaller --onefile --windowed --noconsole --icon=gamble-with-your-friends.ico NoCompatibleToInstall.py
-python -m PyInstaller --onefile --windowed --noconsole --icon=gamble-with-your-friends.ico PlayGame.py
-echo v1.0.4 > GameVersion.txt
+echo "v1.0.4 ModsBuild v1" > GameVersion.txt
